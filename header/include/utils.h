151937d09c128a401a08bf3a02c96c1b4acca9d2
shun-sfoo <ganymede0915@gmail.com>
2025-08-25 21:15:44 +0800

    use gcc 15 and add lab

--- header/include/utils.h
+++ header/include/utils.h
@@ -7,8 +7,8 @@
       char command[1024];                                                     \
       sprintf (command, "echo '%s' > /tmp/csapp_tmp.c", code);                \
       system (command);                                                       \
-      system ("gcc -O" #level " -S /tmp/csapp_tmp.c");                        \
-      system ("cat csapp_tmp.s | sed -n '/.LFB0/,/.LFE0/p'");                 \
+      system ("gcc-15 -O" #level " -S /tmp/csapp_tmp.c");                        \
+      system ("cat csapp_tmp.s | sed -n '/LFB0/,/LFE0/p'");                 \
     }                                                                         \
   while (0);
 
